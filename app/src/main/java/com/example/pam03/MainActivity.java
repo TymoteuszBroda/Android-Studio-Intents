402e629a1633beac4d471c1ac0b640d355495024
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-12-13 23:15:58 +0100

    added new activities

--- app/src/main/java/com/example/pam03/MainActivity.java
+++ app/src/main/java/com/example/pam03/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pam03;
 
+import android.content.ContentUris;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,6 +51,30 @@ public class MainActivity extends AppCompatActivity {
         Uri u = Uri.parse("https://www.ue.katowice.pl/");
         Intent i = new Intent(Intent.ACTION_VIEW, u);
         startActivity(i);
+    }
+
+    public void map(View v) {
+        Uri geoUri = Uri.parse("geo:50.26081,19.04552");
+        Intent i = new Intent(Intent.ACTION_VIEW, geoUri);
+        startActivity(i);
+    }
+
+    public void kalendarz(View v) {
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder,
+                Calendar.getInstance().getTimeInMillis());
+        Intent intent = new Intent(Intent.ACTION_VIEW)
+
+                .setData(builder.build());
+
+        startActivity(intent);
+    }
+
+    public void wlaczAparat(View v) {
+        Intent takePictureIntent = new
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(takePictureIntent);
     }
 
 
