92e1a40467a33965643f41feb3ccb4dd5660131b
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-12-13 22:43:48 +0100

    added method to go to another activity on click

--- app/src/main/java/com/example/pam03/Intencja.java
+++ app/src/main/java/com/example/pam03/Intencja.java
@@ -1,4 +1,21 @@
 package com.example.pam03;
 
-public class Intencja {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Intencja extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intencja);
+
+
+
+
+    }
 }
--- app/src/main/java/com/example/pam03/MainActivity.java
+++ app/src/main/java/com/example/pam03/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pam03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
     }
+
+    public void goToNewActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Intencja.class);
+        startActivity(intent);
+    }
+
+
 }
